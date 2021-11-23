a1a00c872fe5f912be05b4578c20aaf77ebbafc4
29MuhammadRafly <muhammad_rafly_29rpl@student.smktelkom-mlg.sch.id>
2021-11-23 20:09:00 +0700

    first commit

---
+++ PHP Modul 2/latihan7.php
@@ -0,0 +1,13 @@
+<?php
+    $arrBuah = array ("Mangga<br>", "Apel<br>", "Pisang<br>", "Jeruk<br>");
+    echo $arrBuah[0]; //Mangga
+    echo $arrBuah[3]; //Jeruk
+
+    $arrWarna = array();
+    $arrWarna[] = "Merah<br>";
+    $arrWarna[] = "Biru<br>";
+    $arrWarna[] = "Hijau<br>";
+    $arrWarna[] = "Putih<br>";
+        echo $arrWarna[0]; //Merah
+        echo $arrWarna[2]; //Hijau
+?>
